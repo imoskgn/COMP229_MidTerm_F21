d8e73f1e2397fc963b56aa1150f7d1f1facd77c1
Oscar Gonzalez <imoskgn@gmail.com>
2021-10-27 23:46:07 -0400

    Fixing Mongo connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb+srv://admin:admin@cluster0.bxctd.mongodb.net/book_list?retryWrites=true&w=majority"
+  "URI": "mongodb+srv://imoskgn:0fF2S9jFMdmWNkle@cluster0.bxctd.mongodb.net/book_list?retryWrites=true&w=majority"
 };
