c37d9949732fcbb9d87df51e1d92500ea5d61957
Oscar Gonzalez <imoskgn@gmail.com>
2021-10-27 22:21:45 -0400

    Fixing routes, views and db comnnection to MongoDb Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://admin:admin@cluster0.bxctd.mongodb.net/book_list?retryWrites=true&w=majority"
 };
